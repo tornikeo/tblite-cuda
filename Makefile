19f1933e3ba2f5b9fc6a9690a04851bc3d23f830
tornikeo <tornikeonoprishvili@gmail.com>
2025-04-20 23:27:59 +0200

    Add proper limit uses

--- Makefile
+++ Makefile
@@ -24,11 +24,11 @@ INC_FLAGS := $(addprefix -I,$(INC_DIRS))
 
 # The -MMD and -MP flags together generate Makefiles for us!
 # These files will have .d instead of .o as the output.
-CPPFLAGS := $(INC_FLAGS) -MMD -MP
+CPPFLAGS := -G -g $(INC_FLAGS) -MMD -MP 
 
 # The final build step.
 $(BUILD_DIR)/$(TARGET_EXEC): $(OBJS)
-	$(CXX) $(OBJS) -o $@ $(LDFLAGS)
+	$(CXX) -G -g $(OBJS) -o $@ $(LDFLAGS)
 
 # Build step for C source
 $(BUILD_DIR)/%.c.o: %.c
--- src/xtb.cu
+++ src/xtb.cu
@@ -1,16 +1,62 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
+#include <cassert>
+#include <cstdlib>
 #include "xtb.h"
+
+
+
+__device__
+inline void* calloc(size_t nmem, size_t size) {
+  void * ptr = malloc(nmem * size);
+  memset(ptr, 0, nmem * size);
+  return ptr;
+} 
 
 __device__ 
 void xtb_singlepoint(
   const structure_type mol, 
   const xtb_calculator calc, 
   const wavefunction_type wfn,
-  float accuracy,
-  float energy
+  const float accuracy,
+  
+  float energy,
+  float (&gradient)[][3],
+  float (&sigma)[][3],
+  const int verbosity
 ) {
-  printf("We are here.");
+  bool grad, converged, econverged, pconverged;
+  int prlevel;
+  float econv, pconv, cutoff, elast, dpmom[3], qpmom[6], nel;
+  // void *ptr;
+  // real(wp), allocatable :: energies(:), edisp(:), erep(:), exbond(:), eint(:), eelec(:)
+  // real(wp), allocatable :: cn(:), dcndr(:, :, :), dcndL(:, :, :), dEdcn(:)
+  // real(wp), allocatable :: selfenergy(:), dsedcn(:), lattr(:, :), wdensity(:, :, :)
+  // type(integral_type) :: ints
+  // real(wp), allocatable :: tmp(:)
+  // type(potential_type) :: pot
+  // type(container_cache) :: ccache, dcache, icache, hcache, rcache
+  // type(broyden_mixer) :: mixer
+  // type(timer_type) :: timer
+  // type(error_type), allocatable :: 
+  
+  float *energies = (float*)malloc(mol.nat * sizeof(float));
+  assert(energies != NULL);
+  memset(energies, 0, mol.nat * sizeof(float));
+  printf("Thread %d got pointer: %p\n", threadIdx.x, energies);
+  // float *energies = (float*)calloc(mol.nat, sizeof(float));
+  // float *edisp = (float*)calloc(mol.nat, sizeof(float));
+  // float *erep = (float*)calloc(mol.nat, sizeof(float));
+  // float *exbond = (float*)calloc(mol.nat, sizeof(float));
+  // float *eint = (float*)calloc(mol.nat, sizeof(float));
+  // float *eelec = (float*)calloc(mol.nat, sizeof(float));
+  // float *cn = (float*)calloc(mol.nat, sizeof(float));
+  // float *cdndr = (float*)calloc(mol.nat * mol.nat * 3, sizeof(float));
+  
+  // float *dEdcn = (float*)calloc(mol.nat, sizeof(float));
+  // float *selfenergy = (float*)calloc(mol.nat, sizeof(float));
+  // float *tmp = (float*)calloc(mol.nat, sizeof(float));
+  free(energies);
 }
 
 __global__
@@ -19,15 +65,24 @@ void test_xtb_singlepoint()
   structure_type mol = {0};
   xtb_calculator calc = {0};
   wavefunction_type wfn = {0};
+  
+  mol.nat = 9;
+
   float accuracy = 0.01f;
   float energy = 0.0f;
 
-  xtb_singlepoint(mol, calc, wfn, accuracy, energy);
+  float gradient[9][3] = {0};
+  float sigma[3][3] = {0.0};
+  int verbosity = 1;
+
+  xtb_singlepoint(mol, calc, wfn, accuracy, energy, gradient, sigma, verbosity);
   printf("Energy: %f\n", energy);
 }
 
 
 void xtb_test() {
-  test_xtb_singlepoint<<<1,1>>>();
+  cudaDeviceSetLimit(cudaLimitStackSize, 1024 * sizeof(float));
+  cudaDeviceSetLimit(cudaLimitMallocHeapSize, 128*1024*1024);
+  test_xtb_singlepoint<<<10,1>>>();
   cudaDeviceSynchronize();
 }
